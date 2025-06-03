9067459f83ce6c5d439e1606a56edf7df35d011f
Beniahia Alyxandre <alyxandre.beniahia@atmosud.org>
2025-06-03 23:12:14 +0200

    modification de l'indicateur de données corrigées

--- app/js/markerManager.js
+++ app/js/markerManager.js
@@ -172,7 +172,7 @@ function calculateTextParameters(value) {
     let textSize = 32;
     let x_position = 5;
     let y_position = 42;
-    let checkPosition = 'right: 18px;';
+    let checkPosition = 'right: 12px;';
 
     if (value >= 1000) {
         textSize = 16;
@@ -202,10 +202,24 @@ function calculateTextParameters(value) {
 function createTextMarkerHTML(value, textSize, checkPosition, stationData) {
     const hasValue = stationData.valeur !== null;
     const checkIcon = hasValue
-        ? `<i class="bi bi-shield-check" style="position: absolute; top: -13px; ${checkPosition}; font-size: 16px; color:rgb(13, 14, 13);rgba(0,0,0,0.3);"></i>`
+        ? `<span style="
+            position: absolute; 
+            top: -13px; 
+            ${checkPosition}; 
+            background: white; 
+            border-radius: 25px; 
+            display: inline-flex; 
+            align-items: center; 
+            justify-content: center;
+            width: 10px;
+            height: 15px; 
+            box-shadow: 0 0 4px rgba(0,0,0,0.1); 
+            z-index: 10;">
+                <i class="bi bi-shield-fill-check" style="font-size: 18px; color: #0074D9; line-height: 1;"></i>
+          </span>`
         : '';
 
-    return `<div id="textDiv" style="font-size: ${textSize}px; position: relative; display: flex; align-items: center; justify-content: center; width: 100%; height: 100%; color: #333;">
+    return `<div id="textDiv" style="font-size: ${textSize}px; position: relative; display: flex; align-items: center; justify-content: center; width: 100%; height: 100%; color: #333; ">
         ${value}
         ${checkIcon}
     </div>`;
